1004bdd8cbfe280b5690a1e9d038800947fb4cc8
Harold García <supapito85@gmail.com>
2024-11-05 01:29:16 -0500

    organizado

--- static/js/btn.js
+++ static/js/btn.js
@@ -1,6 +1,22 @@
 document.addEventListener('DOMContentLoaded', () => { 
+    const productos = document.querySelectorAll('.compra');
 
-    const producto = document.getElementsByClassName('documento');
+    productos.forEach(btn => {
+        const id = btn.id.split('_')[1];
+        const qrId = "qr_" + id;
+        const codigoqr = document.getElementById(qrId);
+        const cerrarBtn = document.getElementById("cerrar_" + id);
 
-}
-)    
+        btn.addEventListener('click', () => {
+            if (codigoqr) {
+                codigoqr.style.display = 'flex';
+            }
+        });
+
+        cerrarBtn.addEventListener('click', () => {
+            codigoqr.style.display = 'none';
+            window.location.href = '/'
+
+        });
+    });
+});
